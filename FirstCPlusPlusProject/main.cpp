aad7c0499b08d7a22d9fe3fa93710a1b1d716b0b
isaach0011 <ihil17523@740s-pl-121.local>
2017-01-23 12:36:57 -0700

    Created First C++ Project.

---
+++ FirstCPlusPlusProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstCPlusPlusProject
+//
+//  Created by Hill, Isaac on 1/23/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
