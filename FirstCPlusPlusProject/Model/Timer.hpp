5c2bb02cbc66787ff5d2adef956ac52e9c96fb31
isaach0011 <ihil17523@740s-pl-121.local>
2017-01-31 13:36:44 -0700

    Wrote timer file.

--- FirstCPlusPlusProject/Model/Timer.hpp
+++ FirstCPlusPlusProject/Model/Timer.hpp
@@ -21,7 +21,7 @@ public:
     void stopTimer();
     void resetTimer();
     void displayTimerInformation();
-    long getExectionTimeInMicroseconds();
+    long getExecutionTimeInMicroseconds();
 };
 
 #endif /* Timer_hpp */
